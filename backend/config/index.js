cd254adf3596623ed6b1bd18644d1e000cde17f9
hodayaabu <88193085+hodayaabu@users.noreply.github.com>
2024-03-06 22:56:37 +0200

    test

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,11 +1,12 @@
 import configProd from './prod.js'
 import configDev from './dev.js'
 
+
 export var config
 
-if (false && process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
     config = configProd
 } else {
     config = configDev
 }
-config.isGuestMode = true
+config.isGuestMode = true  // ASK - What is it?
